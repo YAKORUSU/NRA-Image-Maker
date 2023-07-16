a7f100aaf65700131c0c897678263404a82fbb57
YAKORUSU <95697718+YAKORUSU@users.noreply.github.com>
2023-07-16 11:51:03 +0900

    Clientの画像取得がPOSTに対応していなかったときのためにGETでも画像を生成できるようにする

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 import make_image
+import json
 
 app = FastAPI()
 
@@ -11,9 +12,17 @@ async def get():
 #Postリクエストが来たら、"make_image"という関数を呼び出し引数としてJsonを渡す
 @app.post("/make_image")
 #postされたJsonを受け取る
-async def post(json: dict):
-    #Jsonを文字列に変換
-    json_str = str(json)
+async def post(request: Request):
+    json_data = await request.json()
+    #jsonを文字列に変換
+    json_str = json.dumps(json_data)
     #make_image.pyのmake_image関数を呼び出し、画像を生成
     return FileResponse(make_image.make_image(json_str))  
+
+#Clientの画像取得がPOSTに対応していなかったときのためにGETでも画像を生成できるようにする
+@app.get("/make_image2/{text}")
+#postされたテキストを受け取る
+async def post(text: str):
+    #make_image.pyのmake_image関数を呼び出し、画像を生成
+    return FileResponse(make_image.make_image(text))  
     
