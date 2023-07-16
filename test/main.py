73ff72297e8621e15f7b51ed4e1cc82c55b1d21d
YAKORUSU <95697718+YAKORUSU@users.noreply.github.com>
2023-07-16 17:08:28 +0900

    Create main.py

---
+++ test/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Request, Query
+from fastapi.responses import FileResponse
+from typing import Optional, Dict
+import json2_image
+import json
+import urllib.parse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get():
+    return {"Result": "Server_ok"}
+
+#Postリクエストが来たら、"make_image"という関数を呼び出し引数としてJsonを渡す
+@app.post("/make_image")
+#postされたJsonを受け取る
+async def post(request: Request):
+    json_data = await request.json()
+    #jsonを文字列に変換
+    json_str = json.dumps(json_data)
+    #make_image.pyのmake_image関数を呼び出し、画像を生成
+    return FileResponse(make_image.make_image(json_str))  
+
+#Clientの画像取得がPOSTに対応していなかったときのためにGETでも画像を生成できるようにする
+@app.get("/make_image2/{text}")
+#postされたテキストを受け取る
+async def post(text: str):
+    #make_image.pyのmake_image関数を呼び出し、画像を生成
+    return FileResponse(make_image.make_image(text))
+
+@app.get("/make_image3/{issue}/{deta}/{place}/{race_number}/{money}/{vote_type}/{horse_number1}/{horse_number2}/{horse_number3}")
+async def make_image(issue: int, deta: str, place: str, race_number: int, money: int, vote_type: int, horse_number1: int, horse_number2: int, horse_number3: int):
+    json_data = {
+        "issue_date": issue,
+        "date": urllib.parse.unquote(f"{deta}"),
+        "place": urllib.parse.unquote(f"{place}"),
+        "race_number": race_number,
+        "money": money,
+        "vote_type": vote_type,
+        "buy": {
+            "horse_number1": horse_number1,
+            "horse_number2": horse_number2,
+            "horse_number3": horse_number3
+        }
+    }
+    print(json_data)
+    json_str = json.dumps(json_data)
+    return FileResponse(json2_image.make_image(json_str))
